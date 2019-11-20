9b6787b723e6bbb6047a1ce794ebb1b6b59a9252
krishna <krishna.tiwari@ignitesol.com>
2019-11-20 15:20:44 +0530

    add another route

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Flask Dockerized'
 
+@app.route('/bye')
+def bye():
+    return 'bye byeeeeeee!'
+
 if __name__ == '__main__':
-    
+
     app.run(debug=True,host='0.0.0.0')
